996c06f424dfb908d9f6d2642b89d30c78667da6
Vera <basketcartoon@yahoo.com>
2024-12-02 00:26:34 +0300

    5-1 task

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,75 @@
 export default class ColumnChart {
+  constructor({
+    data = [],
+    label = '',
+    link = '',
+    value = 0,
+    formatHeading = ''
+  }) {
+    this.data = data;
+    this.label = label;
+    this.link = link;
+    this.value = value;
+    this.formatHeading = formatHeading;
+
+    console.log(this.formatHeading);
+    this.element = this.createElement(this.getTemplate());
+  }
+
+  getLink() {
+    return this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : '';
+  }
+
+  getFormatHeading(value) {
+    return this.formatHeading !== '' ? this.formatHeading(value) : value;
+  }
+
+  getTemplate() {
+    return `<div class="column-chart" style="--chart-height: 50">
+              <div class="column-chart__title">
+                  ${this.label}
+                  ${this.getLink()}
+              </div>
+              <div class="column-chart__container">
+                   ${this.getTemplateContainer(this.data)}
+            </div>`;
+  }
+
+  getTemplateContainer(arr) {
+    if (arr.length === 0) {
+      return `<img src="charts-skeleton.svg" />`;
+    } else {
+      return `<div class="column-chart__header">
+                    ${this.getFormatHeading(this.value)}
+              </div>
+              <div data-element="body" class="column-chart__chart">
+                  ${this.getColumnBody(this.data)}
+              </div`;
+    }
+  }
+
+  getColumnBody(arr) {
+    if (arr.length === 0) {
+      return `<img src="charts-skeleton.svg" />`;
+    }
+
+    let result = '';
+
+    for (let i = 0; i < arr.length; i++) {
+      result += `<div style="--value: ${arr[i]}" ></div>`;
+    }
+
+    return result;
+  }
+
+  createElement(template) {
+    const element = document.createElement(`div`);
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  myFunction() {
+    //console.log(this.label);
+  }
 
 }
