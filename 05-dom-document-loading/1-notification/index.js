35a44f61fddce9e4f8572b3933d01deb441f4864
Vera <basketcartoon@yahoo.com>
2024-12-02 00:27:22 +0300

    5-1 task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,74 @@
 export default class NotificationMessage {
 
+    static lastNatification = null;
+
+    constructor (message = '', { duration, type } = {}) {
+      this.message = message;
+      this.duration = duration;
+      this.type = type;
+      this.element = this.createElement();
+    }
+
+    show(elementForParent = null) {
+      this.destroy();
+      document.body.appendChild(this.element);
+      NotificationMessage.lastNatification = this.element;
+
+      if (elementForParent !== null) {
+        elementForParent.appendChild(this.element);
+        this.element = elementForParent;
+      }
+      this.element.hidden = false;
+      setTimeout(()=>this.remove(), this.duration);
+    }
+
+    remove() {
+      this.element.hidden = true;
+      this.destroy();
+    }
+
+    createElement() {
+      const element = document.createElement('div');
+      element.innerHTML = this.createTemplate();
+      if (NotificationMessage.lastNatification === null) {
+        NotificationMessage.lastNatification = element;
+      }
+      return element.firstElementChild;
+    }
+
+    destroy() {
+      if (NotificationMessage.lastNatification !== null) {
+        let elements = document.querySelectorAll(`div`);
+        for (let elem of elements) {
+          elem.remove(); 
+        }
+        NotificationMessage.lastNatification.remove();
+        NotificationMessage.lastNatification = null;
+      }
+    }
+
+    createTemplate() {
+      return `
+         <div class="${this.getClassForNotification()}" style='--value:${this.duration}ms'> 
+         <div class="timer"></div>
+          <div class="inner-wrapper">
+              <div class="notification-header">
+                  Notification
+              </div>
+              <div class="notification-body">
+                  ${this.message}
+          </div>
+          </div>`;
+    }
+  
+    getClassForNotification() {
+      if (this.type === "success") {
+        return "notification success";
+      } else if (this.type === "error") {
+        return "notification error";
+      } else {
+        return 'notification';
+      }
+    }
+
 }
