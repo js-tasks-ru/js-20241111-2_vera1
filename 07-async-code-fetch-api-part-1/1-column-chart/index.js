6ef21759b4094f089cee85f4facf1c3d6b2cd112
Vera <basketcartoon@yahoo.com>
2025-02-15 19:27:33 +0300

    7-1 Task, 4-1 Task (refactoring + add 1 method)

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -1,7 +1,65 @@
 import fetchJson from './utils/fetch-json.js';
+import ColumnChart1 from '../../04-oop-basic-intro-to-dom/1-column-chart/index.js';
 
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ColumnChart {
+export default class ColumnChart extends ColumnChart1 {
 
+  constructor({
+    url = '',
+    range = {},
+    label = '',
+    link = '',
+    formatHeading = value => value
+  } = {}) {
+    super({label, link, formatHeading});
+    this.url = url;
+    const {from, to} = range;
+    this.fetchData(from, to);
+  }
+
+  createUrl(from, to) {
+    const url = new URL(this.url, BACKEND_URL);
+    url.searchParams.append('from', from);
+    url.searchParams.append('to', to);
+    return url.toString();
+  }
+
+  fetchData(from, to) {
+    fetch(this.createUrl(from, to))
+      .then(response => response.json())
+      .then(data => {
+        this.data = Object.values(data);
+        this.sumOfValuesData(this.data);
+        if (document.getElementById(this.label) != null) {
+          document.getElementById(this.label).innerHTML = this.createTemplate();
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+  async update(from, to) {
+    try {
+      const url = this.createUrl(from, to);
+      const response = await fetchJson(url);
+      this.data = Object.values(response);
+      this.subElements.body.innerHTML = this.createBodyTemplate();
+      if (document.getElementById(this.label) != null) {
+        this.sumOfValuesData(this.data);
+        document.getElementById(this.label).innerHTML = this.createTemplate();
+      }
+      return response;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  sumOfValuesData(data) {
+    let sumValues = 0;
+    for (let value of data) {
+      sumValues = sumValues + value;
+    }
+    this.value = sumValues;
+  }
+  
 }
